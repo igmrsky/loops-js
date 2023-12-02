bc8b0391e296cfd7139b9de98bd25da7d726a030
riskym <112940958+igmrsky@users.noreply.github.com>
2023-12-02 22:32:58 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+for (let i = 2; i <= 12; i = i + 2 ) {
+    console.log(i)
+}
+
+console.log ('For odd number from 2 to 12')
+
+for (let i = 10; i > 0; i--) {
+    console.log (i)
+}
+
+console.log ('For from 10 to 1')
+
+
+i = 0
+
+while(i < 12) {
+    i = i + 2
+    console.log (i)
+}
+
+console.log ('While odd from 2 to 12')
+
+i = 11
+
+while (i > 1) {
+    i = i - 1
+    console.log (i)
+}
+
+console.log ('While from 10 to 1')
